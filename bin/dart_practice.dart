4aa593dc342d208e4fe8dae5b49c162517095519
Arman8957 <lonmaramn0@gmail.com>
2024-02-12 08:46:55 -0330

    Dart intro starting

---
+++ bin/dart_practice.dart
@@ -0,0 +1,34 @@
+
+
+import 'dart:io';
+
+void main() {
+
+  var  a= 50;
+  var b=30;
+  var c= a*b;
+  var authen = false;
+
+  print(c);
+  print(authen);
+  var myList = ["Arman", " Nijam", " Irfan"];
+  print(myList);
+
+  var map_it = {
+    "value1": "Arman",
+    "value2":"My name",
+  };
+
+  print(map_it["value2" ]);
+  print(map_it["value1" ]);
+
+  var my_values = <String>{'happy', 'birhtday', ' to me'};
+  my_values.addAll({'Add another', 'more'});
+
+  my_values.clear();
+  print(my_values);
+
+
+
+
+}
