10ccb5b5bbe5307f86ddf5b5e39cad47b4704fd5
Arman8957 <lonmaramn0@gmail.com>
2024-02-29 14:08:44 -0330

    Class Initiated

--- bin/class_practice_1.dart
+++ bin/class_practice_1.dart
@@ -1,5 +1,46 @@
 class Student{
 
+  String variable_1='';
+  String variable_2='';
 
+//Constructor
+
+  Student(){
+   print('New object created');
+  }
+
+void myFunc(){
+  var a = variable_1 + ' ' +variable_2;
+  print(a);
 
 }
+
+void anotherFunction(){
+  print('$variable_1 write it into it');
+}
+void forObjects(){
+    print('New is still valid');
+}
+
+}
+void main(){
+ List<Student>stduentList= [];
+  Student obj = Student();
+  obj.variable_2 ="Arman";
+  obj.variable_1 ="Hasan";
+  obj.anotherFunction();
+  obj.myFunc();
+
+  stduentList.add(obj);
+  print(stduentList);
+  Student anotherObj = Student();
+
+  anotherObj.anotherFunction();
+
+  var objects = new Student();
+
+  objects.forObjects();
+
+}
+
+
